91a86c6d3d5c85ff163068f3b58a7899df6ca90e
Dapo Momodu <momoduoladapo@gmail.com>
2019-10-18 17:02:25 +0100

    Moved all files from local repo to Github.com

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,73 @@
 import React, {Component} from 'react';
-import NameComponent from './components/NameComponents';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import Select from 'react-select';
+
+
 
 class App extends Component {
   
   constructor(props) {
     super(props);
     this.state = {
-      user_name: 'Victor',
-      profession: 'Programmer'
+      selectedOption: ''
     };
-  }
-
-  componentDidUpdate() {
-    console.log('Changed');
   }
 
   componentDidMount() {
     console.log('Mounted')
   }
-
-  handleClick() {
+  
+  handleChange(selectedOption) {
     this.setState({
-      user_name: "Victor Temprano",
-      profession: 'Academic'
+      selectedOption : selectedOption ? selectedOption : ''
     });
   }
 
   render() {
 
-    const {array} = this.state;
-
-    const style = {fontSize: '40px'};
 
     return (
-      <div className="header">
-        <p style={style}>
-          {this.state.user_name} - {this.state.profession}
-        </p>
-
-        <NameComponent  user_name={this.state.user_name}/>
-        <button onClick={this.handleClick.bind(this)}>
-          <NameComponent />
-        </button>
+      <div>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <h1>Dapo's Page</h1>
+              <p>Here we'll list some data from a bunch of smokin' sources</p>
+              <Select 
+                name="form-field-name"
+                value={this.state.selectedOption.value}
+                onChange={this.handleChange.bind(this)}
+                options={[
+                  {value: 'one', label: 'One'},
+                  {value: 'two', label: 'Two'},
+                ]}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
